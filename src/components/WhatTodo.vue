a4af6bfa607635b053fea69f2a4d66edd86be79b
Karan Jain <karan.jain740@gmail.com>
2023-06-01 12:58:55 +0530

    functional changes

--- src/components/WhatTodo.vue
+++ src/components/WhatTodo.vue
@@ -33,68 +33,63 @@
       </v-card-item>
     </v-card>
 
-    <div class="overlay-menu">
-      <v-menu
-        class="steps-menu"
-        v-model="stepsCard"
-        :close-on-content-click="false"
-      >
-        <v-card class="steps-card">
-          <div class="steps-card-content">
-            <v-card-title class="title pl-0">
-              <span>You're just steps away...</span>
-              <v-icon @click="stepsCard = false" icon="close" />
-            </v-card-title>
-            <v-card-subtitle class="subtitle pl-0">
-              ... to get the best recommendations for your term insurance
-            </v-card-subtitle>
-            <v-divider></v-divider>
-            <v-list>
-              <v-list-item
-                class="steps-list"
-                v-for="(steps, index) in stepsList"
-                :key="index"
-              >
-                <v-list-item-title class="title pb-3">
-                  <span
-                    :class="[
-                      {
-                        'completed-step': steps.completed,
-                      },
-                      index !== 0
-                        ? stepsList[index - 1].completed && !steps.completed
-                          ? 'font-weight-bold'
-                          : ''
-                        : !steps.completed
+    <div class="steps-menu" v-if="stepsCard">
+      <v-card class="steps-card">
+        <div class="steps-card-content">
+          <v-card-title class="title pl-0">
+            <span>You're just steps away...</span>
+            <v-icon @click="stepsCard = false" icon="close" />
+          </v-card-title>
+          <v-card-subtitle class="subtitle pl-0">
+            ... to get the best recommendations for your term insurance
+          </v-card-subtitle>
+          <v-divider></v-divider>
+          <v-list>
+            <v-list-item
+              class="steps-list"
+              v-for="(steps, index) in stepsList"
+              :key="index"
+            >
+              <v-list-item-title class="title pb-3">
+                <span
+                  :class="[
+                    {
+                      'completed-step': steps.completed,
+                    },
+                    index !== 0
+                      ? stepsList[index - 1].completed && !steps.completed
                         ? 'font-weight-bold'
-                        : '',
-                      'd-flex align-center',
-                    ]"
-                    >{{ steps.text }}</span
+                        : ''
+                      : !steps.completed
+                      ? 'font-weight-bold'
+                      : '',
+                    'd-flex align-center',
+                  ]"
+                  >{{ steps.text }}</span
+                >
+                <div>
+                  <v-icon
+                    v-if="steps.completed"
+                    class="check-icon"
+                    icon="check_circle_outline"
+                  />
+                  <v-btn
+                    v-else
+                    variant="outlined"
+                    block
+                    rounded="lg"
+                    class="go-btn"
+                    :disabled="index !== 0 && !stepsList[index - 1].completed"
+                    @click="onCompleteStep(index)"
+                    >Go</v-btn
                   >
-                  <div>
-                    <v-icon
-                      v-if="steps.completed"
-                      class="check-icon"
-                      icon="check_circle_outline"
-                    />
-                    <v-btn
-                      v-else
-                      variant="outlined"
-                      block
-                      rounded="lg"
-                      class="go-btn"
-                      @click="onCompleteStep(index)"
-                      >Go</v-btn
-                    >
-                  </div>
-                </v-list-item-title>
-                <v-divider v-if="index + 1 !== stepsList.length"></v-divider>
-              </v-list-item>
-            </v-list>
-          </div>
-        </v-card>
-      </v-menu>
+                </div>
+              </v-list-item-title>
+              <v-divider v-if="index + 1 !== stepsList.length"></v-divider>
+            </v-list-item>
+          </v-list>
+        </div>
+      </v-card>
     </div>
   </div>
 </template>
